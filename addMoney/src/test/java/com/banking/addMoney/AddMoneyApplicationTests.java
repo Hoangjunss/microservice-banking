2a16b38b3755af5dd0221c206f8bddaef28a0f42
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-12-18 15:16:04 +0700

    add

---
+++ addMoney/src/test/java/com/banking/addMoney/AddMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.addMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ deductMoney/src/test/java/com/banking/deductMoney/DeductMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.deductMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeductMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
